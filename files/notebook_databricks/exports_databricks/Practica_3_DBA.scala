09be168f151d9146c09b5fee8200bbe84a989a1e
pausanchezv <pausanchez.admifin@gmail.com>
2017-12-28 08:38:36 +0100

    filestore

--- files/notebook_databricks/exports_databricks/Practica_3_DBA.scala
+++ files/notebook_databricks/exports_databricks/Practica_3_DBA.scala
@@ -17,8 +17,15 @@
 import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
 import scala.util.Random
 
+// el fitxer d'entrada és el quijote net d'accents i signes que ja s'ha passat pel programa
+// de neteja (la memòria explica com generar-lo)
+val input_file = "/FileStore/tables/clean_quijote.txt"
+
+// fitxer de sortida '.tsv' per visualitzar a tesorflow
+val output_file = "/FileStore/tables/quijote_to_tensorflow.tsv"
+
 // es llegeix el fitxer 
-val quijote = sc.textFile("/FileStore/tables/clean_quijote.txt")
+val quijote = sc.textFile(input_file)
 
 // s'obtenen els tokens (paraules de cada paràgraf)
 val tokens = quijote.map(x => x.split("""\W+"""))
@@ -38,10 +45,12 @@ val model = word2vec.fit(input)
 var stringToTensorflowFile = ""
 
 // es recorren els paràgrafs de l'input ** Aqui es prepara el fitxer '.tsv' com a string
-// el qual després es guardarà com a fitxer ja amb el format admès per tensorflow web
-for (paragraph <- input.take(10)) {
+// el qual després es guardarà com a fitxer ja amb el format admès per tensorflow web.
+// tal i com explica la memòria es visualitzaran molt millor 1000 paràgrafs que els 5000 que té el llibre.
+// si es volen visualitzar tots els paràgrafs només cal canviar '.take(1000)' per '.collect'
+for (paragraph <- input.take(1000)) {
   
-  // cada paràgraf tindrà un vector que serà el conjunt dels vectors de les paraules
+  // cada paràgraf tindrà un vector de característiques format pel conjunt dels vectors de les paraules
   // vectoritzades de tot el paràgraf
   var pVector: Array[String] = Array()
   
@@ -51,10 +60,13 @@ for (paragraph <- input.take(10)) {
   // es recorren les paraules de cada paràgraf
   for (word <- paragraph) {
   
-    // s'aplica la vectorització mitjançanet em mètode 'transform'
-    // per tal de fer-ho cal que la paraula es trobi al vocabulari del model
+    // s'aplica la vectorització mitjançant el mètode 'transform'
+    // per tal de fer-ho cal que la paraula es trobi al vocabulari del model, el bloc
+    // intent/captura ens ajudarà a gestionar-ho
     try {
-      val wordVector = model.transform(word)
+      
+      // vectorització de cada paraula
+      val wordVector = model.transform(word) 
 
       // s'afegeixen els valors de la vectorització al vector de característiques del paràgraf
       for (i <- 0 to wordVector.size - 1) {
@@ -76,7 +88,7 @@ for (paragraph <- input.take(10)) {
 
   // es barreja el vector del paràgraf de manera que els valors de les diferents
   // paraules que conté queden barrejats. Si no es fés així, agafaria sempre els
-  // 100 primers valors corresponents a la primera paraula. D'aquesta amera s'agafen
+  // 100 primers valors corresponents a la primera paraula. D'aquesta manera s'agafen
   // 100 valors abitraris corresponents a les diferents paraules del paràgraf.
   var pList = util.Random.shuffle(pVector.toList)
   pList = pList.slice(0, 100)
@@ -86,17 +98,37 @@ for (paragraph <- input.take(10)) {
     stringToTensorflowFile = stringToTensorflowFile.concat("\t").concat(value.toString)
   }
   
-  // es concatena un salt de línia al final de cada representació
+  // es concatena un salt de línia al final de cada vectorització de paràgraf
   if (pCount > 0) {
     stringToTensorflowFile = stringToTensorflowFile.concat("\n")
   }
 }
 
-// es printegen els 10 primers paràgrafs vectoritzats
-// això és el que caldria convertir a fitxer però com que des de databricks
-// resulta molt complicat desar un fitxer al disc dur, s'ha realitzat l'exemple sencer
-// des del projecte d'IntellijIdea adjuntat a la pràctica.
-// Tal i com s'explica a la memòria executant el projecte (que és exactament aquest codi però enlloc
-// de fer print desa l'string a disc com a fitxer TSV) es pot obtenir el fitxer TSV per tensor flow.
-// Si us plau, cal seguir les instruccions de la memòria!
-println(stringToTensorflowFile)
+// s'elimina el fitxer de sortida del dbfs si existeix
+dbutils.fs.rm(output_file)
+
+// es crea el fitxer de sortida tsv omplert amb l'string de sortida
+dbutils.fs.put(output_file, stringToTensorflowFile)
+
+/*
+ ********* LLEGIR AIXÔ SI US PLAU **********
+
+Es pot comprovar que el '.tsv' s'ha generat correctament executant la següent instrucció: 
+display(dbutils.fs.ls("/FileStore/tables"))
+
+Ara és possible descarregar-se el fitxer copiant la segëunt URL, enganxant-la a la barra del navegador i executant-la
+Alerta, però, amb el paràmetre o=1519882038357773, ja que pertany a la sessió de DataBricks de Pau Sanchez. Per descarregar
+el fitxer el lector ha d'alçar la mirada a la URL del notebook, canviar el paràmetre pel seu i llavors executar la nova
+URL amb el seu paràmetre personal:
+
+https://community.cloud.databricks.com/files/tables/quijote_to_tensorflow.tsv?o=1519882038357773
+
+Això funciona correctament, ara bé, per si el lector tingués algun tipus de problema per descarregar-se el fitxer, aquest
+fitxer ja es troba generat a l'entrega. La memòria n'explica la localització exacta.
+
+A més, per si això no fós suficient, s'ha preparat un projecte d'Intellij que també genera el fitxer de sortida '.tsv'
+*/
+
+// COMMAND ----------
+
+display(dbutils.fs.ls("/FileStore/tables"))
